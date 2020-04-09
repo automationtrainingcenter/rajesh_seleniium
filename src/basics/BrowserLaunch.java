544c01dc1def06e9e4f21a694d50fd8e3b016357
automationtrainingcenter <atcsurya@gmail.com>
2020-04-09 08:48:43 +0530

    initial upload with browser launch code

---
+++ src/basics/BrowserLaunch.java
@@ -0,0 +1,24 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserLaunch {
+	public static void main(String[] args) {
+		
+		// Set the System property to the path of chromedriver executable
+		System.setProperty("webdriver.chrome.driver", "/Users/suryaprakash/Documents/softwares/chromedriver");
+		
+		// Create an object of ChromeDriver class to open Chrome browser
+		ChromeDriver cdriver = new ChromeDriver();
+		
+		
+		// set system property to the path of geckodriver executable
+		System.setProperty("webdriver.gecko.driver", "/Users/suryaprakash/Documents/softwares/geckodriver");
+		
+		// Create an object of FirefoxDriver class to open firefox browser
+		FirefoxDriver fdriver = new FirefoxDriver();
+		
+	}
+
+}
